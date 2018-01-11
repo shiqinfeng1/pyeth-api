88d7f3600a655f6d7999a7a1dace0415c27d00da
shiqinfeng1 <150627601@qq.com>
2018-01-11 21:57:52 +0800

    add twitter.sol

---
+++ smart_contracts/twitter.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.4.19;
+
+contract Owned {
+    /// @dev `owner` is the only address that can call a function with this
+    /// modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner) ;
+        _;
+    }
+
+    address public owner;
+
+    /// @notice The Constructor assigns the message sender to be `owner`
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+    address public newOwner;
+
+    /// @notice `owner` can step down and assign some other address to this role
+    /// @param _newOwner The address of the new owner. 0x0 can be used to create
+    ///  an unowned neutral vault, however that cannot be undone
+    function changeOwner(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+
+    function acceptOwnership() public {
+        if (msg.sender == newOwner) {
+            owner = newOwner;
+        }
+    }
+}
+
+
+contract account is  Owned {
+
+    event Log_lotus(bytes32 _id, bytes32[] users);
+    event Log_bind_account(bytes32 _id, address _addr);
+    event Log_unbind_account(bytes32 _id);
+    event Log_lotus_result(bytes32 _id, bytes32[] users,address[] users_addr);
+    
+    mapping(bytes32 => address) public accounts; //用户绑定的地址列表
+    mapping(bytes32 => bytes32[]) public retweeters; //保存retweet_id对应的转发用户列表
+    /*
+    luckyboys_num: 中奖用户数目
+    retweet_id: 推文id
+    users_list:  所有转发的用户id 
+    */
+    function lotus(uint luckyboys_num, bytes32 retweet_id, bytes32[] users_list) public onlyOwner {
+        
+        bytes32[] memory valid_users_list; //转发的用户id中,已绑定地址的用户
+        address[] memory users_addr; //转发的用户id中,已绑定地址的用户的以太坊地址
+        bytes32[] memory luckyboys;  //中奖用户id
+        address[] memory luckyboys_addr; //中奖用户地址
+        
+        retweeters[retweet_id] = users_list; //记录该推文在当前时间点被哪些用户转发
+        
+        Log_lotus(retweet_id,users_list);
+        
+        //从所有转发的用户中,过滤已绑定地址的用户,并记录他们的绑定地址
+        for(uint i=0;i<users_list.length;i++){
+            if(accounts[users_list[i]]!=0){
+                users_addr[i] = accounts[users_list[i]];
+                valid_users_list[i] = users_list[i];
+            }
+        }
+        //随机抽取中奖用户
+        uint total_users = users_addr.length;
+        for(uint j=0;j<luckyboys_num;j++){
+            //addmod(a,b,c) 取模算法: (a+b)%c
+            uint temp = addmod(uint(block.blockhash(block.number-j)),j,total_users);
+            luckyboys_addr[j] = users_addr[temp];
+            luckyboys[j] = valid_users_list[temp];
+        }
+        Log_lotus_result(retweet_id,luckyboys,luckyboys_addr);
+    }
+    function bind_account(bytes32 _id, address _addr) public onlyOwner {
+        accounts[_id] = _addr;
+        Log_bind_account( _id,  _addr);
+    }
+     function unbind_account(bytes32 _id) public onlyOwner {
+        delete accounts[_id];
+        Log_unbind_account( _id);
+    }
+}
